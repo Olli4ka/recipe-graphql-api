1b7c8a35a4974418ec34ff2ab9cd8b6d9ec8ca57
Olga Panayot <panayot.olga@gmail.com>
2026-08-25 23:52:47 +0300

    feat: customize review and favorite admin

--- src/app_recipe/admin/favorite.py
+++ src/app_recipe/admin/favorite.py
@@ -1,4 +1,43 @@
 from django.contrib import admin
+
 from ..models import Favorite
 
-admin.site.register(Favorite)
+
+@admin.register(Favorite)
+class FavoriteAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "user",
+        "recipe",
+        "created_at",
+    )
+
+    list_display_links = (
+        "id",
+        "recipe",
+    )
+
+    search_fields = (
+        "user__username",
+        "recipe__title",
+    )
+
+    list_filter = ("created_at",)
+
+    ordering = ("-created_at",)
+
+    list_per_page = 10
+
+    autocomplete_fields = (
+        "user",
+        "recipe",
+    )
+
+    readonly_fields = ("created_at",)
+
+    date_hierarchy = "created_at"
+
+    list_select_related = (
+        "user",
+        "recipe",
+    )
--- src/app_recipe/admin/review.py
+++ src/app_recipe/admin/review.py
@@ -1,4 +1,48 @@
 from django.contrib import admin
+
 from ..models import Review
 
-admin.site.register(Review)
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "recipe",
+        "user",
+        "rating",
+        "created_at",
+    )
+
+    list_display_links = (
+        "id",
+        "recipe",
+    )
+
+    search_fields = (
+        "recipe__title",
+        "user__username",
+        "comment",
+    )
+
+    list_filter = (
+        "rating",
+        "created_at",
+    )
+
+    ordering = ("-created_at",)
+
+    list_per_page = 10
+
+    autocomplete_fields = (
+        "recipe",
+        "user",
+    )
+
+    readonly_fields = ("created_at",)
+
+    date_hierarchy = "created_at"
+
+    list_select_related = (
+        "recipe",
+        "user",
+    )

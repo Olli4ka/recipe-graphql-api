4b3e9a8a56e0a6d32949cef733cb09de468a9b4e
Olga Panayot <panayot.olga@gmail.com>
2026-08-25 21:08:33 +0300

    feat: customize category and ingredient admin

--- src/app_recipe/admin/category.py
+++ src/app_recipe/admin/category.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
+
 from ..models import Category
 
-admin.site.register(Category)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "slug")
+    list_display_links = ("id", "name")
+    search_fields = ("name", "slug")
+    ordering = ("name",)
+    list_per_page = 10
--- src/app_recipe/admin/ingredient.py
+++ src/app_recipe/admin/ingredient.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
+
 from ..models import Ingredient
 
-admin.site.register(Ingredient)
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ("id", "name")
+    list_display_links = ("id", "name")
+    search_fields = ("name",)
+    ordering = ("name",)
+
+    list_per_page = 10

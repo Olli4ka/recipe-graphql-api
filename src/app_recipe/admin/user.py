75b8552140937ab0015b1f186d7b89649cd25034
Olga Panayot <panayot.olga@gmail.com>
2026-08-25 20:54:49 +0300

    feat: customize user admin

--- src/app_recipe/admin/user.py
+++ src/app_recipe/admin/user.py
@@ -1,4 +1,44 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
 from ..models import User
 
-admin.site.register(User)
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    list_display = (
+        "id",
+        "username",
+        "email",
+        "first_name",
+        "last_name",
+        "is_staff",
+    )
+    list_display_links = (
+        "id",
+        "username",
+    )
+    search_fields = (
+        "username",
+        "email",
+        "first_name",
+        "last_name",
+    )
+    list_filter = (
+        "is_staff",
+        "is_superuser",
+        "is_active",
+    )
+    ordering = ("username",)
+    list_per_page = 10
+    fieldsets = BaseUserAdmin.fieldsets + (
+        (
+            "Profile",
+            {
+                "fields": (
+                    "avatar",
+                    "bio",
+                )
+            },
+        ),
+    )

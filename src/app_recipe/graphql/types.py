b9eb32d952db9e22341ad4812ef7c5ec3dafca0d
Olga Panayot <panayot.olga@gmail.com>
2026-08-27 13:31:06 +0300

    feat: add GraphQL types

---
+++ src/app_recipe/graphql/types.py
@@ -0,0 +1,67 @@
+import graphene
+from graphene_django import DjangoObjectType
+
+from ..models import (
+    Category,
+    Favorite,
+    Ingredient,
+    Recipe,
+    RecipeIngredient,
+    Review,
+    Step,
+    User
+)
+
+
+class UserType(DjangoObjectType):
+    class Meta:
+        model = User
+        fields = "__all__"
+
+
+class CategoryType(DjangoObjectType):
+    class Meta:
+        model = Category
+        fields = "__all__"
+
+
+class IngredientType(DjangoObjectType):
+    class Meta:
+        model = Ingredient
+        fields = "__all__"
+
+
+class RecipeType(DjangoObjectType):
+    class Meta:
+        model = Recipe
+        fields = "__all__"
+
+
+class RecipeIngredientType(DjangoObjectType):
+    class Meta:
+        model = RecipeIngredient
+        fields = "__all__"
+
+
+class StepType(DjangoObjectType):
+    class Meta:
+        model = Step
+        fields = "__all__"
+
+
+class ReviewType(DjangoObjectType):
+    class Meta:
+        model = Review
+        fields = "__all__"
+
+
+class FavoriteType(DjangoObjectType):
+    class Meta:
+        model = Favorite
+        fields = "__all__"
+
+
+class RecipeListType(graphene.ObjectType):
+    total_count = graphene.Int()
+    has_next_page = graphene.Boolean()
+    items = graphene.List(RecipeType)
